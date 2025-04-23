c569992cbc8fac9814dc213429d286ee001cf2e6
giglifiore <49178731+giglifiore@users.noreply.github.com>
2025-04-23 10:40:08 -0400

    Update urls.py

--- miapp/urls.py
+++ miapp/urls.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.urls import path
-
+from home.views import hola
 def home(request):
     return HttpResponse("Hola mundo desde Django en Render!")
 
 urlpatterns = [
     path('', home),
+    path('', hola),
 ]
